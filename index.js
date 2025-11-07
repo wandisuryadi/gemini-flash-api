f98ae7cf502200357131c6b19810883e6365d948
Wandi Suryadi <wandi229703@gmail.com>
2025-11-07 15:01:34 +0700

    gemini-flash-api
    gemini-flash-api

---
+++ index.js
@@ -0,0 +1,93 @@
+import 'dotenv/config';
+import express from 'express';
+import multer from 'multer';
+import fs from 'fs/promises';
+import { GoogleGenAI } from '@google/genai';
+
+const app = express();
+const upload = multer();
+const ai = new GoogleGenAI({ apiKey: process.env.OPENAI_API_KEY });
+
+const GEMINI_MODEL = "gemini-2.5-flash";
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server ready on http://localhost:${PORT}`));
+
+app.post('/generate-text', async (req, res) => {
+    const { prompt } = req.body;
+
+    try {
+        const response = await ai.models.generateContent({
+            model: GEMINI_MODEL,
+            contents: prompt
+        });
+
+        res.status(200).json({ result: response.text });
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({ message: e.message });
+    }
+});
+
+app.post('/generate-from-image', upload.single('image'), async (req, res) => {
+    const { prompt } = req.body;
+    const base64Image = req.file.buffer.toString('base64');
+
+    try {
+        const response = await ai.models.generateContent({
+            model: GEMINI_MODEL,
+            contents:[
+                { text: prompt },
+                { inlineData: {data: base64Image, mimeType: req.file.mimetype}}
+            ],
+        });
+
+        res.status(200).json({ result: response.text });
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({ message: e.message });
+    }
+});
+
+app.post('/generate-from-document', upload.single('document'), async (req, res) => {
+    const { prompt } = req.body;
+    const base64Document = req.file.buffer.toString('base64');
+
+    try {
+        const response = await ai.models.generateContent({
+            model: GEMINI_MODEL,
+            contents:[
+                { text: prompt ?? "Tolong buat ringkas dari dokumen berikut: ", type: "text" },
+                { inlineData: {data: base64Document, mimeType: req.file.mimetype}}
+            ],
+        });
+
+        res.status(200).json({ result: response.text });
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({ message: e.message });
+    }
+});
+
+app.post('/generate-from-audio', upload.single('audio'), async (req, res) => {
+    const { prompt } = req.body;
+    const base64Audio = req.file.buffer.toString('base64');
+
+    try {
+        const response = await ai.models.generateContent({
+            model: GEMINI_MODEL,
+            contents:[
+                { text: prompt ?? "Tolong transkrip dari rekaman berikut: ", type: "text" },
+                { inlineData: {data: base64Audio, mimeType: req.file.mimetype}}
+            ],
+        });
+
+        res.status(200).json({ result: response.text });
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({ message: e.message });
+    }
+});
